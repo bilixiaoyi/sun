ba409ca4e12b3c75386491c42e1f4e7c37542f29
bilixiaoyi <164715024+bilixiaoyi@users.noreply.github.com>
2024-10-20 15:48:59 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,9 @@
+function openINDEX() {
+    window.location.href = "index.html";
+}
+function one() {
+    window.location.href = "one.html";
+}
+function two() {
+    window.location.href = "two.html";
+}
